bf1989d7684c8a3b3eb01a1f18c5af81e8ddd0fe
unknown <59097122+noyyyy@users.noreply.github.com>
2023-10-16 16:48:44 +0800

    chore: delete codegen

--- contracts/src/systems/IncrementSystem.sol
+++ contracts/src/systems/IncrementSystem.sol
@@ -3,15 +3,15 @@ pragma solidity >=0.8.0;
 
 import {console} from "forge-std/console.sol";
 import {System} from "@latticexyz/world/src/System.sol";
-import {CounterTable} from "../codegen/Tables.sol";
+import {Counter} from "../codegen/Tables.sol";
 
 contract IncrementSystem is System {
     error MyCustomError();
 
     function increment() public returns (uint32) {
-        uint32 counter = CounterTable.get();
+        uint32 counter = Counter.get();
         uint32 newValue = counter + 1;
-        CounterTable.set(newValue);
+        Counter.set(newValue);
         return newValue;
     }
 }
